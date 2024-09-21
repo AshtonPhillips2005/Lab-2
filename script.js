eb6a0709a1b94cb54ff4ee86c63930ee35086bb3
AshtonPhillips2005 <ashtongamer11@gmail.com>
2024-09-21 16:10:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -10,35 +10,35 @@ else{
 }
 
 
-var day = 'Sunday';
+var day = 1;
 
 switch(day){
 
-    case 'Sunday':
+    case 1:
         console.log("Today is Sunday.");
     break;
 
-    case 'Monday':
+    case 2:
         console.log("Today is Monday");
     break;
 
-    case 'Tuesday':
+    case 3:
         console.log("Today is Tuesday.");
     break;
 
-    case 'Wednesday':
+    case 4:
         console.log("Today is wednesday.");
     break;
 
-    case 'Thursday':
+    case 5:
         console.log("Today is thursday.");
     break;
 
-    case 'Friday':
+    case 6:
         console.log("Today is Friday.");
     break;
 
-    case 'Saturday':
+    case 7:
         console.log("Today is Saturday.");
     break;
 }
